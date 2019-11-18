ff70e357ce790c16a18b83217e57bb72afa61e64
Ramprit Sahani <rampritsahani@protonmail.com>
2019-11-18 22:30:39 +0530

    adding user

--- models/user.js
+++ models/user.js
@@ -1,11 +1,31 @@
 const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: { type: String, required: true, minlength: 5, maxlength: 255 },
+  name: { type: String, required: true, minlength: 5, maxlength: 50 },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
   image: { type: String, default: "https://via.placeholder.com/150" }
 });
 
 module.exports = mongoose.model("User", UserSchema);
+module.exports = function(user) {
+  const schema = Joi.object().keys({
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  });
+  return Joi.validate(user, schema);
+};
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const Product = require("../models/product");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.status(200).json({ message: "get products" });
+  const products = await Product.find();
+  res.status(200).json(products);
+});
+
+router.post("/", async (req, res, next) => {
+  const { name, description, image, price, published } = req.body;
+  const product = new Product({
+    name,
+    description,
+    image,
+    price,
+    published
+  });
+  const result = await product.save();
+  res.status(201).json(result);
 });
 
 module.exports = router;
